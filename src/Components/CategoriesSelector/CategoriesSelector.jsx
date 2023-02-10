5a8f6d851eae8031a6cea4a9d8c35086a1dc0ed4
Juan Carlos M Giner <jcmginer@gmail.com>
2023-02-10 09:39:53 +0100

    Style

--- src/Components/CategoriesSelector/CategoriesSelector.jsx
+++ src/Components/CategoriesSelector/CategoriesSelector.jsx
@@ -17,11 +17,12 @@ const CategoriesSelector = () => {
         <div className="search-container">
             <form onSubmit={handleSubmit}>
                 <h2>Select Pokemon GIF category</h2>
-                <button value='pikachu' onClick={() => setSearchTerm('pikachu')} type="submit">Pikachu</button>
-                <button value='bulbasaur' onClick={() => setSearchTerm('bulbasaur')} type="submit">Bulbasaur</button>
-                <button value='squirtle' onClick={() => setSearchTerm('squirtle')} type="submit">Squirtle</button>
-                <button value='charmander' onClick={() => setSearchTerm('charmander')} type="submit">Charmander</button>
-                <br />
+                <div className="btn-container">
+                <button className="btn-search" value='pikachu' onClick={() => setSearchTerm('pikachu')} type="submit">Pikachu</button>
+                <button className="btn-search" value='bulbasaur' onClick={() => setSearchTerm('bulbasaur')} type="submit">Bulbasaur</button>
+                <button className="btn-search" value='squirtle' onClick={() => setSearchTerm('squirtle')} type="submit">Squirtle</button>
+                <button className="btn-search" value='charmander' onClick={() => setSearchTerm('charmander')} type="submit">Charmander</button>
+                </div>
                 <div className="result-container">
                     {searchResults.map((result) => (
                         <img key={result.id} src={result.images.downsized_medium.url} alt={searchResults.name} width="150px" />
